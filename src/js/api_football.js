c34ccd7ccf47c065c857f1bc099484b1de5bceaa
Chandra Septian <cst_dev155@unitedtractors.com>
2023-01-02 16:25:52 +0700

    fixing coding format & adjust request load

--- src/js/api_football.js
+++ src/js/api_football.js
@@ -1,12 +1,12 @@
 import {
-   addFavorite,
-   deleteFavorite,
-   checkData,
-   getFavData,
-} from './db_football';
+  addFavorite,
+  deleteFavorite,
+  checkData,
+  getFavData,
+} from "./db_football";
 
-const base_url = 'https://api.football-data.org/v2/';
-const token = '';
+const base_url = "https://api.football-data.org/v2/";
+const token = "5652e38754df4fe5ae30adf26099d344";
 const inggris = 2021;
 const perancis = 2015;
 
@@ -15,101 +15,101 @@ const ligue_standing = `${base_url}competitions/${perancis}/standings`;
 const team_detail = `${base_url}teams/`;
 
 const fetchApi = (url) => {
-   return fetch(url, {
-      method: 'get',
-      mode: 'cors',
-      headers: {
-         'X-Auth-Token': token,
-      },
-   })
-      .then(status)
-      .then(json)
-      .catch(error);
+  return fetch(url, {
+    method: "get",
+    mode: "cors",
+    headers: {
+      "X-Auth-Token": token,
+    },
+  })
+    .then(status)
+    .then(json)
+    .catch(error);
 };
 
 const status = (response) => {
-   if (response.status !== 200) {
-      console.log('Error : ' + response.status);
-      return Promise.reject(new Error(response.statusText));
-   } else {
-      return Promise.resolve(response);
-   }
+  if (response.status !== 200) {
+    console.log("Error : " + response.status);
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    return Promise.resolve(response);
+  }
 };
 
 const json = (response) => {
-   return response.json();
+  return response.json();
 };
 
 const error = (error) => {
-   console.log('Error : ' + error);
+  console.log("Error : " + error);
 };
 
 const getStandings = (league) => {
-   showLoader();
-   if ('caches' in window) {
-      caches.match(league).then((response) => {
-         if (response) {
-            response.json().then((data) => {
-               showStandings(data);
-            });
-         }
-      });
-   }
+  console.log("url", league);
+  showLoader();
+  if ("caches" in window) {
+    caches.match(league).then((response) => {
+      if (response) {
+        response.json().then((data) => {
+          showStandings(data);
+        });
+      }
+    });
+  }
 
-   fetchApi(league).then((data) => {
-      showStandings(data);
-   });
+  fetchApi(league).then((data) => {
+    showStandings(data);
+  });
 };
 
 const showStandings = (data) => {
-   hideLoader();
-   let standingsHTML = '';
-   data.standings[0].table.forEach((dt) => {
-      standingsHTML += `
-                <td width="10%" class="center">${dt.position}</td>
-                <td><a href="team.html?id=${dt.team.id}"><img src="${dt.team.crestUrl}" onError="this.onerror=null;this.src='/images/default.png';" class="responsive-img" style="margin: 0 15px -3px 0; width:40px; height:40px">${dt.team.name}</a></td>
-                <td>${dt.playedGames}</td>
-                <td>${dt.won}</td>
-                <td>${dt.draw}</td>
-                <td>${dt.lost}</td>
-                <td>${dt.goalDifference}</td>
-                <td><b>${dt.points}</b></td>
-              </tr>
-          `;
-   });
-   document.getElementById('standings').innerHTML = standingsHTML;
+  let standingsHTML = "";
+  data.standings[0].table.forEach((dt) => {
+    standingsHTML += `
+      <tr>
+         <td width="10%" class="center">${dt.position}</td>
+         <td><a href="team.html?id=${dt.team.id}"><img src="${dt.team.crestUrl}" onError="this.onerror=null;this.src='/images/default.png';" class="responsive-img" style="margin: 0 15px -3px 0; width:40px; height:40px">${dt.team.name}</a></td>
+         <td>${dt.playedGames}</td>
+         <td>${dt.won}</td>
+         <td>${dt.draw}</td>
+         <td>${dt.lost}</td>
+         <td>${dt.goalDifference}</td>
+         <td><b>${dt.points}</b></td>
+      </tr>
+      `;
+  });
+  document.getElementById("standings").innerHTML = standingsHTML;
 };
 
 const getTeam = () => {
-   const urlParams = new URLSearchParams(window.location.search);
-   const idParam = urlParams.get('id');
-   const team = `${team_detail}${idParam}`;
+  const urlParams = new URLSearchParams(window.location.search);
+  const idParam = urlParams.get("id");
+  const team = `${team_detail}${idParam}`;
 
-   showLoader();
-   if (idParam !== null) {
-      if ('caches' in window) {
-         caches.match(team).then((response) => {
-            if (response) {
-               response.json().then((data) => {
-                  showTeam(data);
-                  buttonAction(data);
-               });
-            }
-         });
-      }
-
-      fetchApi(team).then((data) => {
-         showTeam(data);
-         buttonAction(data);
+  showLoader();
+  if (idParam !== null) {
+    if ("caches" in window) {
+      caches.match(team).then((response) => {
+        if (response) {
+          response.json().then((data) => {
+            showTeam(data);
+            buttonAction(data);
+          });
+        }
       });
-   }
+    }
+
+    fetchApi(team).then((data) => {
+      showTeam(data);
+      buttonAction(data);
+    });
+  }
 };
 
 const showTeam = (data) => {
-   hideLoader();
-   let teamHTML = '';
+  let teamHTML = "";
 
-   teamHTML += `<img src=${data.crestUrl.replace(/^http:\/\//i, 'https://')} 
+  teamHTML += `<img src=${data.crestUrl.replace(/^http:\/\//i, "https://")} 
    onError="this.onerror=null;this.src='/images/default.png';" 
    width="180" height="180" class="responsive-img center">
    <br>
@@ -121,7 +121,7 @@ const showTeam = (data) => {
       <br>
    <br> `;
 
-   teamHTML += `
+  teamHTML += `
    <table class="responsive-table highlight" width=500>
       <thead class="indigo lighten-4">
          <tr>
@@ -132,74 +132,73 @@ const showTeam = (data) => {
       </thead>
    <tbody>`;
 
-   data.squad.forEach((player) => {
-      teamHTML += `
+  data.squad.forEach((player) => {
+    teamHTML += `
       <tr>
          <td>${player.name}</td>
          <td>${player.position}</td>
          <td>${player.nationality}</td>
       </tr>
       `;
-   });
+  });
 
-   teamHTML += '</tbody></table>';
+  teamHTML += "</tbody></table>";
 
-   document.getElementById('team-detail').innerHTML = teamHTML;
+  document.getElementById("team-detail").innerHTML = teamHTML;
 };
 
 const getFavoriteTeam = () => {
-   const dbData = getFavData();
-   dbData.then((data) => {
-      let timBodyHtml = '';
-      if (data.length > 0) {
-         hideLoader();
-         data.forEach((team) => {
-            timBodyHtml += `
+  const dbData = getFavData();
+  dbData.then((data) => {
+    let timBodyHtml = "";
+    if (data.length > 0) {
+      data.forEach((team) => {
+        timBodyHtml += `
             <div>
               <a href="team.html?id=${team.id}"><b>${team.name}</b></a><br>
               <img src=${team.crestUrl.replace(
-                 /^http:\/\//i,
-                 'https://'
+                /^http:\/\//i,
+                "https://"
               )} alt="" class="responsive-img" width="220">
             </div>
             <br><br>`;
-         });
-      } else {
-         timBodyHtml = '<h6>Tidak ada Tim Favorite yang ditambahkan</h6>';
-      }
-      document.getElementById('favoriteBody').innerHTML = timBodyHtml;
-   });
+      });
+    } else {
+      timBodyHtml = "<h6>Tidak ada Tim Favorite yang ditambahkan</h6>";
+    }
+    document.getElementById("favoriteBody").innerHTML = timBodyHtml;
+  });
 };
 
 const buttonAction = (data) => {
-   let btnSave = document.getElementById('btnSave');
-   let btnDelete = document.getElementById('btnDelete');
+  let btnSave = document.getElementById("btnSave");
+  let btnDelete = document.getElementById("btnDelete");
 
-   btnSave.onclick = () => {
-      addFavorite(data);
-      btnSave.style.display = 'none';
-      btnDelete.style.display = 'block';
-   };
+  btnSave.onclick = () => {
+    addFavorite(data);
+    btnSave.style.display = "none";
+    btnDelete.style.display = "block";
+  };
 
-   btnDelete.onclick = () => {
-      deleteFavorite(data);
-      btnSave.style.display = 'block';
-      btnDelete.style.display = 'none';
-   };
+  btnDelete.onclick = () => {
+    deleteFavorite(data);
+    btnSave.style.display = "block";
+    btnDelete.style.display = "none";
+  };
 
-   checkData(data.id)
-      .then(() => {
-         btnSave.style.display = 'none';
-         btnDelete.style.display = 'block';
-      })
-      .catch(() => {
-         btnSave.style.display = 'block';
-         btnDelete.style.display = 'none';
-      });
+  checkData(data.id)
+    .then(() => {
+      btnSave.style.display = "none";
+      btnDelete.style.display = "block";
+    })
+    .catch(() => {
+      btnSave.style.display = "block";
+      btnDelete.style.display = "none";
+    });
 };
 
 const showLoader = () => {
-   const loader = `<div class="preloader-wrapper big active">
+  const loader = `<div class="preloader-wrapper big active">
     <div class="spinner-layer spinner-blue-only" >
       <div class="circle-clipper left">
         <div class="circle"></div>
@@ -211,17 +210,17 @@ const showLoader = () => {
     </div>
   </div>`;
 
-   document.getElementById('loader').innerHTML = loader;
+  document.getElementById("loader").innerHTML = loader;
 };
 
 const hideLoader = () => {
-   document.getElementById('loader').innerHTML = '';
+  document.getElementById("loader").innerHTML = "";
 };
 
 export {
-   getTeam,
-   getStandings,
-   getFavoriteTeam,
-   premier_standing,
-   ligue_standing,
+  getTeam,
+  getStandings,
+  getFavoriteTeam,
+  premier_standing,
+  ligue_standing,
 };
--- src/js/register-push.js
+++ src/js/register-push.js
@@ -1,91 +1,86 @@
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+import runtime from "serviceworker-webpack-plugin/lib/runtime";
 
 function urlBase64ToUint8Array(base64String) {
-   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-   const base64 = (base64String + padding)
-      .replace(/-/g, '+')
-      .replace(/_/g, '/');
-   const rawData = window.atob(base64);
-   const outputArray = new Uint8Array(rawData.length);
-   for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-   }
-   return outputArray;
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 // Periksa service worker
-if (!('serviceWorker' in navigator)) {
-   console.log('Service worker tidak didukung browser ini.');
+if (!("serviceWorker" in navigator)) {
+  console.log("Service worker tidak didukung browser ini.");
 } else {
-   registerServiceWorker();
-   requestPermission();
+  registerServiceWorker();
+  requestPermission();
 }
 // Register service worker
 function registerServiceWorker() {
-   const registration = runtime.register();
-   registration
-      .then(function () {
-         console.log('Registrasi service worker berhasil.');
-      })
-      .catch(function (err) {
-         console.error('Registrasi service worker gagal.', err);
-      });
+  const registration = runtime.register();
+  registration
+    .then(function () {
+      console.log("Registrasi service worker berhasil.");
+    })
+    .catch(function (err) {
+      console.error("Registrasi service worker gagal.", err);
+    });
 }
 function requestPermission() {
-   if ('Notification' in window) {
-      Notification.requestPermission().then(function (result) {
-         if (result === 'denied') {
-            console.log('Fitur notifikasi tidak diijinkan.');
-            return;
-         } else if (result === 'default') {
-            console.error('Pengguna menutup kotak dialog permintaan ijin.');
-            return;
-         }
+  if ("Notification" in window) {
+    Notification.requestPermission().then(function (result) {
+      if (result === "denied") {
+        console.log("Fitur notifikasi tidak diijinkan.");
+        return;
+      } else if (result === "default") {
+        console.error("Pengguna menutup kotak dialog permintaan ijin.");
+        return;
+      }
 
-         navigator.serviceWorker.ready.then(() => {
-            if ('PushManager' in window) {
-               navigator.serviceWorker
-                  .getRegistration()
-                  .then(function (registration) {
-                     registration.pushManager
-                        .subscribe({
-                           userVisibleOnly: true,
-                           applicationServerKey: urlBase64ToUint8Array(
-                              'BLnkgazyt5LAUVMdIv89RdRBRHA7RVQ1TSAJqirgFMU6dE78vRndwFoHewpvZUd8hAOBW_Yu1FFemrZ8UFq7NmA'
-                           ),
-                        })
-                        .then(function (subscribe) {
-                           console.log(
-                              'Berhasil melakukan subscribe dengan endpoint: ',
-                              subscribe.endpoint
-                           );
-                           console.log(
-                              'Berhasil melakukan subscribe dengan p256dh key: ',
-                              btoa(
-                                 String.fromCharCode.apply(
-                                    null,
-                                    new Uint8Array(subscribe.getKey('p256dh'))
-                                 )
-                              )
-                           );
-                           console.log(
-                              'Berhasil melakukan subscribe dengan auth key: ',
-                              btoa(
-                                 String.fromCharCode.apply(
-                                    null,
-                                    new Uint8Array(subscribe.getKey('auth'))
-                                 )
-                              )
-                           );
-                        })
-                        .catch(function (e) {
-                           console.error(
-                              'Tidak dapat melakukan subscribe ',
-                              e.message
-                           );
-                        });
-                  });
-            }
-         });
+      navigator.serviceWorker.ready.then(() => {
+        if ("PushManager" in window) {
+          navigator.serviceWorker
+            .getRegistration()
+            .then(function (registration) {
+              registration.pushManager
+                .subscribe({
+                  userVisibleOnly: true,
+                  applicationServerKey: urlBase64ToUint8Array(
+                    "BLnkgazyt5LAUVMdIv89RdRBRHA7RVQ1TSAJqirgFMU6dE78vRndwFoHewpvZUd8hAOBW_Yu1FFemrZ8UFq7NmA"
+                  ),
+                })
+                .then(function (subscribe) {
+                  console.log(
+                    "Berhasil melakukan subscribe dengan endpoint: ",
+                    subscribe.endpoint
+                  );
+                  console.log(
+                    "Berhasil melakukan subscribe dengan p256dh key: ",
+                    btoa(
+                      String.fromCharCode.apply(
+                        null,
+                        new Uint8Array(subscribe.getKey("p256dh"))
+                      )
+                    )
+                  );
+                  console.log(
+                    "Berhasil melakukan subscribe dengan auth key: ",
+                    btoa(
+                      String.fromCharCode.apply(
+                        null,
+                        new Uint8Array(subscribe.getKey("auth"))
+                      )
+                    )
+                  );
+                })
+                .catch(function (e) {
+                  console.error("Tidak dapat melakukan subscribe ", e.message);
+                });
+            });
+        }
       });
-   }
+    });
+  }
 }
